5fec84b98cfc64f4a66b03385ea512c9fa6ef0f5
Keith Phu <keith@ewebdevelopment.com>
2016-08-25 17:09:12 -0700

    Revert "prevent public/system symlink from creation"
    This reverts commit 45aab15aba8fec3e35ceb7846594b7d16ccfc63f.

--- deploy/attributes/customize.rb
+++
@@ -1,5 +0,0 @@
-node[:deploy].each do |application, deploy|
-  normal[:deploy][application][:purge_before_symlink] = ['log', 'tmp/pids']
-  normal[:deploy][application][:symlinks] = {"pids" => "tmp/pids", "log" => "log"}
-end
-
